f0e998bf5766d40b726ba89bc2cfcfca28fd2de5
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-10-21 20:11:54 +0330

    Update BackedBy.ts

--- src/app/copy/BackedBy.ts
+++ src/app/copy/BackedBy.ts
@@ -197,7 +197,7 @@ export const BackedByCopy = {
         url: "https://x.com/marcelharmann",
       },
       {
-        src: "./img/backed-by/matt.jpeg",
+        src: "/img/backed-by/matt.jpeg",
         title: "Matt - CTO <br> THORWallet Co-Founder",
         twitter: "@mattGabriel",
         github: "",
