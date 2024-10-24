fcc8ac295d04f000053fd1bf43612830f90ca654
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-10-24 09:56:50 +0330

    Fix bg color

--- src/app/shared-components/NavBar.tsx
+++ src/app/shared-components/NavBar.tsx
@@ -48,7 +48,8 @@ export function NavBar() {
         label: link.name,
         children: link.children.map((child, ind) => ({
           key: `${index}-${ind}`,
-          label: <Link href={child.url} className="childli" >{child.name} </Link>,
+          style:{backgroundColor: "#031635"},
+          label: <Link href={child.url} >{child.name} </Link>,
         })),
       });
     } else {
