404e54a4553e7312c2be9125548e1fe0ea7e8957
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-10-24 08:29:22 +0330

    Fix backby

--- src/app/components/backedby/index.tsx
+++ src/app/components/backedby/index.tsx
@@ -6,10 +6,52 @@ export function Sections() {
     <>
       <h1
         id="page-title"
-        className="monserrat-bold text-uppercase text-center mx-auto"
+        className="monserrat-bold color-neutral text-uppercase text-center mx-auto"
         dangerouslySetInnerHTML={{ __html: BackedByCopy.title }}
       ></h1>
-      {/* <!-- FUNDS --> */}
+      {/* <!-- Seed-Investors --> */}
+      <div id="segment-4" className="blue-section w-100 text-center mt-5">
+        <h3 className="gradient-text mx-auto monserrat-bold">
+          {BackedByCopy["segment-4"].title}
+        </h3>
+        <div className="card-cont mt-5">
+          {BackedByCopy["segment-4"].cards.map((item, index) => (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              className="card-item p-3 my-4 text-white "
+            >
+              <Image
+                className="avatar"
+                width={100}
+                height={100}
+                src={item.src}
+                alt="21e8 Capital"
+              />
+              <h6
+                dangerouslySetInnerHTML={{ __html: item.title }}
+                className="menlo text-bold mt-2 "
+              ></h6>
+              <div className="text-white d-flex">
+                <span>
+                  <Image
+                    src={`/img/backed-by/${item.iconImg}`}
+                    alt=""
+                    width={25}
+                    height={25}
+                  />
+                </span>
+                <u
+                  dangerouslySetInnerHTML={{ __html: item.twitter }}
+                  className="menlo "
+                ></u>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+      {/* <!-- Seed-Investors --> */}
       <div id="segment-1" className="blue-section w-100 text-center mt-5">
         <h3 className="gradient-text mx-auto monserrat-bold">
           {BackedByCopy["segment-1"].title}
@@ -29,14 +71,14 @@ export function Sections() {
                 src={item.src}
                 alt="21e8 Capital"
               />
-              <h5
+              <h6
                 dangerouslySetInnerHTML={{ __html: item.title }}
                 className="menlo text-bold mt-2 "
-              ></h5>
-              <div className=" text-white">
+              ></h6>
+              <div className=" text-white d-flex">
                 <span>
                   <Image
-                    src="/img/backed-by/icon-x.png"
+                    src="/img/backed-by/twitter.svg"
                     alt="X"
                     width={25}
                     height={25}
@@ -51,49 +93,6 @@ export function Sections() {
           ))}
         </div>
       </div>
-      {/* <!-- FOUNDERS --> */}
-      <div id="segment-2" className="blue-section w-100 text-center mt-5">
-        <h3 className="gradient-text mx-auto monserrat-bold">
-          {BackedByCopy["segment-2"].title}
-        </h3>
-        <div className="card-cont mt-5">
-          {BackedByCopy["segment-2"].cards.map((item, index) => (
-            <a
-              key={index}
-              href={item.url}
-              target="_blank"
-              className="card-item p-3 my-4 text-white"
-            >
-              <Image
-                className="avatar"
-                src={item.src}
-                width={100}
-                height={100}
-                alt="JPTHOR"
-              />
-              <h5
-                dangerouslySetInnerHTML={{ __html: item.title }}
-                className="menlo text-bold mt-2"
-              ></h5>
-              <div className="text-underline text-white">
-                <span>
-                  <Image
-                    src="/img/backed-by/icon-x.png"
-                    alt="X"
-                    width={25}
-                    height={25}
-                  />
-                </span>
-                <u
-                  dangerouslySetInnerHTML={{ __html: item.twitter }}
-                  className="menlo"
-                ></u>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
-
       {/* <!-- KOLS --> */}
       <div id="segment-3" className="blue-section w-100 text-center mt-5 ">
         <h3 className="gradient-text mx-auto monserrat-bold">
@@ -114,10 +113,10 @@ export function Sections() {
                 height={100}
                 alt="ThorTrades"
               />
-              <h5
+              <h6
                 dangerouslySetInnerHTML={{ __html: item.title }}
                 className="menlo text-bold mt-2"
-              ></h5>
+              ></h6>
               <div className="text-underline text-white d-flex">
                 <span>
                   <Image
@@ -136,7 +135,7 @@ export function Sections() {
           ))}
         </div>
       </div>
-      {/* <!-- Seed Investors --> */}
+      {/* <!-- FOUNDERS --> */}
       <div id="segment-2" className="blue-section w-100 text-center my-5">
         <h3 className="gradient-text mx-auto monserrat-bold">
           {BackedByCopy["segment-2"].title}
